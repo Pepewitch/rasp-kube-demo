4fb49ba4301b60b59b84278916c4303553def04f
Witchayut <skpepe134@gmail.com>
2019-12-01 21:13:29 +0700

    add build

---
+++ rasp-frontend/client/build/precache-manifest.6596ac9ed4c880c63d4ee7bb990acc38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "238a335b01e15b3f610a3c4cc0c54c08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "988ebbbd77d3ea37404c",
+    "url": "/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "9dde0a394eb9d9db8100",
+    "url": "/static/css/main.fa58898a.chunk.css"
+  },
+  {
+    "revision": "988ebbbd77d3ea37404c",
+    "url": "/static/js/2.1bf401a1.chunk.js"
+  },
+  {
+    "revision": "9dde0a394eb9d9db8100",
+    "url": "/static/js/main.d85d386a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
