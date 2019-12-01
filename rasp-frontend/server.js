e2c94c444d352f1c005091b4c9192d82f2f5c443
Witchayut <skpepe134@gmail.com>
2019-12-01 21:05:50 +0700

    update frontend server

--- rasp-frontend/server.js
+++ rasp-frontend/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
-const axios = require("axios");
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.get("*", (req, res) => {
+  return res
+    .status(200)
+    .sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(process.env.PORT || 3000);
